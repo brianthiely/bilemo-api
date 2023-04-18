18a57beefe3154760fa6fb8c004937ca98a74957
Brian <brian.thiely@gmail.com>
2023-04-18 12:17:57 +0200

    Fix: All issues ExceptionSubscriber

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -10,30 +10,31 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
+
+
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
 
         if ($exception instanceof HttpException) {
             $data = [
-                'status' => $exception->getStatusCode(),
-                'message' => $exception->getMessage()
-            ];
-
-        } else {
-            $data = [
-                'status' => 500,
-                'message' => $exception->getMessage()
-            ];
-
+                     'status' => $exception->getStatusCode(),
+                     'message' => $exception->getMessage()
+                 ];
+            $event->setResponse(new JsonResponse($data));
+            return;
         }
+
+        $data = [
+            'status' => 500,
+            'message' => $exception->getMessage()
+        ];
         $event->setResponse(new JsonResponse($data));
     }
 
+
     public static function getSubscribedEvents(): array
     {
-        return [
-            KernelEvents::EXCEPTION => 'onKernelException',
-        ];
+        return [KernelEvents::EXCEPTION => 'onKernelException'];
     }
 }
